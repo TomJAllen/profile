85f9a62e97c491586a8a988bc5311fa877266199
Tom Allen <tomallen_1@hotmail.co.uk>
2023-05-02 15:21:03 +1000

    added game dev pages

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
 class Header extends HTMLElement {
   connectedCallback() {
-    this.innerHTML = `
-            <nav>
-    <a href="index.html"><h2>TOM ALLEN</h2></a>
-    <ul>
-      <li><a href="about_me.html">About Me</a></li>
-      <li><a href="index.html">Projects</a></li>
-      <li><a href="cv.html">CV</a></li>
-      <li><a href="contact_me.html">Contact Me</a></li>
-    </ul>
-  </nav>
-        `
+    this.innerHTML =
+      `
+        <nav>
+          <a href="index.html"><h2>TOM ALLEN</h2></a>
+          <ul>
+            <li><a href="about_me.html">About Me</a></li>
+            <li><a href="index.html">Projects</a></li>
+            <li><a href="cv.html">CV</a></li>
+            <li><a href="contact_me.html">Contact Me</a></li>
+          </ul>
+        </nav>
+      `
   }
 }
 
