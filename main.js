059368c457dfc5938ae7112c54b70a57dc4dc35a
Tom Allen <tomallen_1@hotmail.co.uk>
2023-05-02 17:15:10 +1000

    added mobile nav

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ class Header extends HTMLElement {
   connectedCallback() {
     this.innerHTML =
       `
-        <nav>
+        <nav id="header">
           <a href="index.html"><h2>TOM ALLEN</h2></a>
           <ul>
             <li><a href="about_me.html">About Me</a></li>
@@ -15,6 +15,30 @@ class Header extends HTMLElement {
   }
 }
 
+class MobileHeader extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML =
+      `
+        <div id="mobile-nav">
+
+          <a href="index.html"><h2>TOM ALLEN</h2></a>
+
+          <div id="myLinks">
+            <a href="about_me.html">About Me</a>
+            <a href="index.html">Projects</a>
+            <a href="cv.html">CV</a>
+            <a href="contact_me.html">Contact Me</a>
+          </div>
+
+          <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+            <i class="fa fa-bars"></i>
+          </a>
+
+      </div>
+      `
+  }
+}
+
 //Footer
 
 class Footer extends HTMLElement {
@@ -22,11 +46,22 @@ class Footer extends HTMLElement {
     this.innerHTML =
       `
         <div id=footer>
-          <a id="footer-name" href="../index.html"><strong>Tom Allen |</strong> Product Management & Web Development
+          <a id="footer-name" href="contact_me.html"><strong>Tom Allen |</strong> Product Management & Web Development
           </a>
         </div>
       `
   }
 }
+
 customElements.define('main-nav', Header);
+customElements.define('mobile-nav', MobileHeader);
 customElements.define('main-footer', Footer);
+
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
