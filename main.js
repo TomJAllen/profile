6b5e9b80383d7cca7f56fb84ab7fde36d4b08880
Tom Allen <thomasjamesallen91@gmail.com>
2024-07-04 21:50:09 +1000

    updated front page

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ class MobileHeader extends HTMLElement {
 
           <a href="index.html"><h2>TOM ALLEN</h2></a>
 
-          <div id="my-links">
+          <div id="my-links" style="display:none">
             <a href="about_me.html">About Me</a>
             <a href="index.html#projects-overview">Projects</a>
             <a href="cv.html">CV</a>
@@ -65,3 +65,60 @@ function changeNav() {
     x.style.display = "block";
   }
 }
+
+// Switching words
+
+var words = document.getElementsByClassName('word');
+var wordArray = [];
+var currentWord = 0;
+
+words[currentWord].style.opacity = 1;
+for (var i = 0; i < words.length; i++) {
+  splitLetters(words[i]);
+}
+
+function changeWord() {
+  var cw = wordArray[currentWord];
+  var nw = currentWord == words.length - 1 ? wordArray[0] : wordArray[currentWord + 1];
+  for (var i = 0; i < cw.length; i++) {
+    animateLetterOut(cw, i);
+  }
+
+  for (var i = 0; i < nw.length; i++) {
+    nw[i].className = 'letter behind';
+    nw[0].parentElement.style.opacity = 1;
+    animateLetterIn(nw, i);
+  }
+
+  currentWord = (currentWord == wordArray.length - 1) ? 0 : currentWord + 1;
+}
+
+function animateLetterOut(cw, i) {
+  setTimeout(function () {
+    cw[i].className = 'letter out';
+  }, i * 80);
+}
+
+function animateLetterIn(nw, i) {
+  setTimeout(function () {
+    nw[i].className = 'letter in';
+  }, 340 + (i * 80));
+}
+
+function splitLetters(word) {
+  var content = word.innerHTML;
+  word.innerHTML = '';
+  var letters = [];
+  for (var i = 0; i < content.length; i++) {
+    var letter = document.createElement('span');
+    letter.className = 'letter';
+    letter.innerHTML = content.charAt(i);
+    word.appendChild(letter);
+    letters.push(letter);
+  }
+
+  wordArray.push(letters);
+}
+
+changeWord();
+setInterval(changeWord, 4000);
